79eea5d876ae413604042d7dd5cfb693352aa12d
Matt Bernes <mattbernes128@gmail.com>
2025-02-04 20:31:11 -0500

    working postgres db

---
+++ cicdpractice/airflow-docker/dags/load_to_postgres.py
@@ -0,0 +1,38 @@
+from airflow import DAG
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+
+
+def load_data_to_postgres(**kwargs):
+	postgres_hook = PostgresHook(postgres_conn_id="postgres_default") # Your connection ID
+	sql = """
+		CREATE TABLE IF NOT EXISTS my_table (
+			id INT PRIMARY KEY,
+			name VARCHAR(255)
+		);
+		INSERT INTO my_table (id, name) VALUES (1, 'Airflow Data');
+	"""
+	postgres_hook.run(sql)
+
+default_args = {
+	'owner': 'airflow',
+	'depends_on_past': False,
+	'email_on_failure': False,
+	'email_on_retry': False,
+	'retries': 1,
+}
+
+
+with DAG(
+	dag_id="postgres_example",
+	default_args=default_args,
+	start_date=datetime(2023, 10, 27),
+	schedule=None,  # Run manually or set a schedule
+	catchup=False,
+) as dag:
+	load_data = PythonOperator(
+		task_id="load_data",
+		python_callable=load_data_to_postgres,
+	)
+
